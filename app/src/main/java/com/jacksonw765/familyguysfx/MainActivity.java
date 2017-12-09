36cf13c6f6e1f4f77232ff2647eef25a18466271
jacksksonLaptop <welchjk@mail.uc.edu>
2017-12-08 20:57:18 -0500

    added updates

--- app/src/main/java/com/jacksonw765/familyguysfx/MainActivity.java
+++ app/src/main/java/com/jacksonw765/familyguysfx/MainActivity.java
@@ -1,13 +1,32 @@
 package com.jacksonw765.familyguysfx;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        button = (Button)this.findViewById(R.id.TaDa);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.tada);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
     }
 }
