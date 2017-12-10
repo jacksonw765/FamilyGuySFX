a588649828f56e037a93a4056f8831225d405152
jacksksonLaptop <welchjk@mail.uc.edu>
2017-12-09 23:15:51 -0500

    Quagmire

--- app/src/main/java/com/jacksonw765/familyguysfx/MainActivity.java
+++ app/src/main/java/com/jacksonw765/familyguysfx/MainActivity.java
@@ -5,29 +5,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
     //peter buttons
-    public Button buttonPeterTada;
-    public Button buttonPeterCares;
-    public Button buttonPeterMeg;
-    public Button buttonPeterRoadhouse;
+    private Button buttonPeterTada;
+    private Button buttonPeterCares;
+    private Button buttonPeterMeg;
+    private Button buttonPeterRoadhouse;
 
     //bruce buttons
-    public Button buttonBruceOhNo;
-    public Button buttonBruceAnus;
-    public Button buttonBruceDoThat;
-    public Button buttonBruceLetsTalk;
-    public Button buttonBruceLiveAlone;
-    public Button buttonBrucePullingLegs;
-    public Button buttonBruceThatsAlright;
-    public Button buttonBruceOhHey;
+    private Button buttonBruceOhNo;
+    private Button buttonBruceAnus;
+    private Button buttonBruceDoThat;
+    private Button buttonBruceLetsTalk;
+    private Button buttonBruceLiveAlone;
+    private Button buttonBrucePullingLegs;
+    private Button buttonBruceThatsAlright;
+    private Button buttonBruceOhHey;
+
+    //quagmire buttons
+    private Button buttonQuagmireArousing;
+    private Button buttonQuagmireAwful;
+    private Button buttonQuagmireFatChicks;
+    private Button buttonQuagmireGiggidi;
+    private Button buttonQuagmireHeardBefore;
+    private Button buttonQuagmireNeatIdea;
+    private Button buttonQuagmireParkingTicket;
+    private Button buttonQuagmireWaHey;
+
+    private AdView adview;
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = new RelativeLayout(this);
+        adview = this.findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder()
+                .setGender(AdRequest.GENDER_MALE)
+                .setIsDesignedForFamilies(false)
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        adview.loadAd(request);
 
         //peter init
         buttonPeterTada = this.findViewById(R.id.TaDa);
@@ -44,6 +73,16 @@ public class MainActivity extends AppCompatActivity {
         buttonBruceLetsTalk = this.findViewById(R.id.button_bruce_letstalk);
         buttonBruceAnus = this.findViewById(R.id.button_bruce_anus);
         buttonBruceDoThat = this.findViewById(R.id.button_bruce_dothat);
+
+        //quagmire init
+        buttonQuagmireArousing = this.findViewById(R.id.button_quagmire_arousing);
+        buttonQuagmireAwful = this.findViewById(R.id.button_quagmire_awful);
+        buttonQuagmireFatChicks = this.findViewById(R.id.button_quagmire_fat);
+        buttonQuagmireGiggidi = this.findViewById(R.id.button_quagmire_giggidi);
+        buttonQuagmireHeardBefore = this.findViewById(R.id.button_quagmire_heardbefore);
+        buttonQuagmireNeatIdea = this.findViewById(R.id.button_quagmire_neatidea);
+        buttonQuagmireParkingTicket = this.findViewById(R.id.button_quagmire_parkingticket);
+        buttonQuagmireWaHey = this.findViewById(R.id.button_quagmire_whaaahey);
 
         //peter listeners
         buttonPeterTada.setOnClickListener(new View.OnClickListener() {
@@ -86,9 +125,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         //bruce listeners
-
-
         buttonBruceOhNo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -163,6 +201,87 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.bruce_ohhey);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        //Quagmire Listeners
+        buttonQuagmireWaHey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_whaaaaheeehey);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        buttonQuagmireArousing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_arousing);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        buttonQuagmireAwful.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_awful);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        buttonQuagmireFatChicks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_fatchicks);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        buttonQuagmireGiggidi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_giggidi);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        buttonQuagmireHeardBefore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_heardbefore);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        buttonQuagmireNeatIdea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_neatidea);
+                if(!mp.isPlaying()) {
+                    mp.start();
+                }
+            }
+        });
+
+        buttonQuagmireParkingTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quagmire_parkingticket);
                 if(!mp.isPlaying()) {
                     mp.start();
                 }
